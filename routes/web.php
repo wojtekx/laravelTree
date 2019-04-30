13f55bf983e1af4aee30a056ad4376f8a64bf70f
wkogut93 <w.kogut@ideo.pl>
2019-04-30 15:35:40 +0200

    second commit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::resource('contacts', 'ContactController');
 Route::resource('tree', 'TreeController');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('category-tree-view',['uses'=>'CategoryController@manageCategory']);
+Route::post('add-category',['as'=>'add.category','uses'=>'CategoryController@addCategory']);
